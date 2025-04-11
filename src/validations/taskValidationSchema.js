836de73594515661656dba248ab445751855008f
shahsuresh <shahsuresh75@gmail.com>
2025-04-11 10:06:15 +0545

    Task Validation Schema using Joi

---
+++ src/validations/taskValidationSchema.js
@@ -0,0 +1,26 @@
+import Joi from "joi";
+
+const newTaskValidationSchema = Joi.object({
+  title: Joi.string()
+    .max(100)
+    .empty("")
+    .messages({
+      "string.max": "Title cannot be more than 100 characters",
+    })
+    .default("Untitled Task")
+    .trim(),
+  description: Joi.string()
+    .max(1000)
+    .required()
+    .messages({
+      "any.required": "Description is Required123", // when field is missing
+      "string.empty": "Description Field cannot be empty", // when it's an empty string ""
+      "string.max": "Description cannot exceed 1000 characters",
+    })
+    .trim(),
+  status: Joi.string()
+    .default("Pending")
+    .valid("Pending", "In-Progress", "Completed"),
+});
+
+export default newTaskValidationSchema;
