90ec34953a7a259ea61720508bcb06539a9c48bf
shahsuresh <shahsuresh75@gmail.com>
2025-04-11 10:07:20 +0545

    New task validation Schema added to validate req.body data

--- src/controllers/task.routes.js
+++ src/controllers/task.routes.js
@@ -1,23 +1,31 @@
 import { Router } from "express";
 import Task from "../models/task.model.js";
+import validateReqBodyData from "../middleware/data.validation.middleware.js";
+import newTaskValidationSchema from "../validations/taskValidationSchema.js";
 
 const router = Router();
 
 //# route to create new task
 
-router.post("/create", async (req, res) => {
-  //extract new task data from req.body
-  const newTaskData = req.body;
+router.post(
+  "/create",
+  validateReqBodyData(newTaskValidationSchema),
+  async (req, res) => {
+    //extract new task data from req.body
+    const newTaskData = req.body;
 
-  try {
-    await Task.create(newTaskData);
-    res.status(200).send({ message: "New Task Created Successfully" });
-  } catch (error) {
-    res
-      .status(400)
-      .send({ message: "Error creating new task", error: error.message });
+    try {
+      //add new task to db
+      await Task.create(newTaskData);
+      //send response
+      return res.status(200).send({ message: "New Task Created Successfully" });
+    } catch (error) {
+      res
+        .status(400)
+        .send({ message: "Error creating new task", error: error.message });
+    }
   }
-});
+);
 
 //# route to get all tasks list
 
