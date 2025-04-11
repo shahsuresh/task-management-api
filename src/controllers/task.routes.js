1a7ed69b52d1cede3002309cf7fa346bcacce1bc
shahsuresh <shahsuresh75@gmail.com>
2025-04-11 13:42:06 +0545

    api to get delete task by id and update task by id added

--- src/controllers/task.routes.js
+++ src/controllers/task.routes.js
@@ -2,13 +2,15 @@ import { Router } from "express";
 import Task from "../models/task.model.js";
 import validateReqBodyData from "../middleware/data.validation.middleware.js";
 import newTaskValidationSchema from "../validations/taskValidationSchema.js";
+import validateMongoIdFromReqParams from "../middleware/validateMongoID.middleware.js";
+import updateTaskStatusValidationSchema from "../validations/updateTaskStatusValidationSchema.js";
 
 const router = Router();
 
-//# route to create new task
+//# api to create new task
 
 router.post(
-  "/create",
+  "/add",
   validateReqBodyData(newTaskValidationSchema),
   async (req, res) => {
     //extract new task data from req.body
@@ -27,7 +29,7 @@ router.post(
   }
 );
 
-//# route to get all tasks list
+//# api to get all tasks list
 
 router.get("/task-list", async (req, res) => {
   //get task list from db
@@ -40,4 +42,89 @@ router.get("/task-list", async (req, res) => {
   return res.status(200).send({ tasks: taskList });
 });
 
+//# api to Retrieve a specific task by its id
+
+router.get("/:id", validateMongoIdFromReqParams, async (req, res) => {
+  //retrieve task id from params
+  const taskId = req.params.id;
+
+  //find task with that id
+  const task = await Task.findOne({ _id: taskId });
+
+  //if not task available with that id, throw error
+  if (!task) {
+    return res.status(404).send({ message: "Task Not Found" });
+  }
+
+  //send response
+  return res.status(200).send({ message: "Success", taskDetails: task });
+});
+
+//# api to delete a task by ID
+
+router.delete("/delete/:id", validateMongoIdFromReqParams, async (req, res) => {
+  //retrieve task id from params
+  const taskId = req.params.id;
+
+  //find task with that id
+  const task = await Task.findOne({ _id: taskId });
+
+  //if not task available with that id, throw error
+  if (!task) {
+    return res.status(404).send({ message: "Task Not Found" });
+  }
+
+  // if task with this id exists, delete it
+  await Task.deleteOne({ _id: taskId });
+
+  //send response
+  return res.status(200).send({ message: "Task Deleted Successfully" });
+});
+
+//# api to update task by id
+
+router.put(
+  "/update/:id",
+  validateMongoIdFromReqParams,
+  validateReqBodyData(newTaskValidationSchema),
+  async (req, res) => {
+    //retrieve task id from params
+    const taskId = req.params.id;
+
+    //find task with that id
+    const task = await Task.findOne({ _id: taskId });
+
+    //if not task available with that id, throw error
+    if (!task) {
+      return res.status(404).send({ message: "Task Not Found" });
+    }
+
+    // if task with this id exists, update with new details
+
+    // extract new data from req.body
+    const updatedTaskData = req.body;
+    // console.log(updatedTaskData);
+
+    // update task
+    await Task.updateOne({ _id: taskId }, { $set: { ...updatedTaskData } });
+
+    //send response
+    return res.status(200).send({ message: "Task Updated" });
+  }
+);
+
+//# api to update task status only
+
+router.patch(
+  "/:id/status",
+  validateMongoIdFromReqParams,
+  validateReqBodyData(updateTaskStatusValidationSchema),
+  async (req, res) => {
+    //extract id from req params
+    const taskId = req.params.id;
+
+    console.log(taskId);
+    return res.status(200).send({ taskID: taskId });
+  }
+);
 export default router;
